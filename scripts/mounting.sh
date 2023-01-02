2affb5ed152ac7034c963760c2f6a6045c58353e
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-03 02:25:15 +0300

    Update mounting.sh

--- scripts/mounting.sh
+++ scripts/mounting.sh
@@ -7,6 +7,9 @@ mount /dev/sdb1 ./newROOT
 
 echo "mountin /boot into /newROOT.."
 if [ -d "newROOT/boot" ]; then
+	echo "+mounitng point exists"
+else
+	echo "creating mounitng point.."
 	mkdir ./newROOT/boot
 fi
 mount /dev/sda1 ./newROOT/boot
