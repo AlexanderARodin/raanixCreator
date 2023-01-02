e8b207953b531d225e1642efdbb8bc0b4269cb45
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-03 02:56:23 +0300

    Update netlink.sh

--- scripts/netlink.sh
+++ scripts/netlink.sh
@@ -11,4 +11,4 @@ else
 	mkdir /home/netexch
 fi
 
-mount -t cifs //192.168.3.2 /home/netexch -o user=Xxxxxxxxx,password=Xxxxx,file_mode=0777,dir_mode=0777
+mount -t cifs //192.168.3.2/home /home/netexch -o user=Xxxxxxxxx,password=Xxxxx,file_mode=0777,dir_mode=0777
