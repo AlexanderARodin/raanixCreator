3ce512713758679a4a4ddae050daeede260a3ebc
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-03 02:54:11 +0300

    Update netlink.sh

--- scripts/netlink.sh
+++ scripts/netlink.sh
@@ -4,5 +4,11 @@ set -e
 
 apt install -y cifs-utils
 
-mkdir /home/netexch
-mount -t cifs //192.168.3.2 /home/netexch -o user=Xxxxxxxx,password=Xxxxx,file_mode=0777,dir_mode=0777
+if [ -d "/home/netexch" ]; then
+	echo "+"
+else
+	echo ".."
+	mkdir /home/netexch
+fi
+
+mount -t cifs //192.168.3.2 /home/netexch -o user=Xxxxxxxxx,password=Xxxxx,file_mode=0777,dir_mode=0777
