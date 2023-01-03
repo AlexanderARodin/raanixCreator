709de9eb6dcb5543aa83d3f385a70c00d4826e43
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-01-03 03:28:02 +0300

    Update netlink.sh

--- scripts/netlink.sh
+++ scripts/netlink.sh
@@ -12,5 +12,5 @@ else
 fi
 
 echo "try to link.."
-mount -t cifs //192.168.3.2/home /home/netexch -o user=Xxxxxxxxx,password=Xxxxx,file_mode=0777,dir_mode=0777
+mount -t cifs //192.168.3.2/home /home/netexch -o user=$1,password=$2,file_mode=0777,dir_mode=0777
 echo "..ok?"
